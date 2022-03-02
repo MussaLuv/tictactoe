b3660f3d09f741e0351ee12eb56383c03ff183ab
Aaron Reyes <ProdAronBeatz@gmail.com>
2022-03-02 14:14:24 -0600

    board now updating in state

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 /* first have to find how to make many div inside the div in the html
 const div = document.querySelector('div')
 Put all the variables of X and O towards the outer scope 
@@ -16,72 +15,74 @@ instead of changing the color, want to put an X or an O inside of each of
  the cells*/
 
 const gameState = {
-    players: ['X', 'O'],
-    board: [
-      [null, null, null],
-      [null, null, null],
-      [null, null, null]
+  players: ["X", "O"],
+  currentPlayerIndex: 0,
+  board: [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ],
+};
 
+// gameState.board[0][0] = 'x'
+// gameState.board[0][1] = 'o'
+// gameState.board[0][2] = 'x'
+// gameState.board[1][0] = 'x'
+// gameState.board[1][1] = 'o'
+// gameState.board[1][2] = 'x'
+// gameState.board[2][0] = 'x'
+// gameState.board[2][1] = 'o'
+// gameState.board[2][2] = 'x'
 
-    ]
+const gameZone = document.querySelector("table");
+
+gameZone.addEventListener("click", function (event) {
+  if (event.target.nodeName === "TD") {
+    let row = event.target.id[0];
+    let column = event.target.id[2];
+
+    console.log({ row, column });
+
+    gameState.board[row][column] =
+      gameState.players[gameState.currentPlayerIndex];
+
+    if (gameState.currentPlayerIndex === 0) {
+      gameState.currentPlayerIndex = 1;
+    } else if (gameState.currentPlayerIndex === 1) {
+      gameState.currentPlayerIndex = 0;
+    }
   }
 
-
-  gameState.board[0][0] = 'x'
-  gameState.board[0][1] = 'o'
-  gameState.board[0][2] = 'x'
-  gameState.board[1][0] = 'x' 
-  gameState.board[1][1] = 'o'   
-  gameState.board[1][2] = 'x'
-  gameState.board[2][0] = 'x'
-  gameState.board[2][1] = 'o'
-  gameState.board[2][2] = 'x'
-  
-  console.log(gameState) 
-
-
-const gameZone = document.getElementById("tacArea");
-gameZone.addEventListener ("click", function (event) {
-
-  if(event.target.matches) {
-    event.target.classList.insert("x")
-  }
-
+  console.log(gameState.board);
 });
 
+var button = document.createElement("button");
+button.innerHTML = "click";
+document.body.appendChild(button);
 
 // gameState.board.addEventListener("click", function (event) {
-  // console.log(event.target)
+// console.log(event.target)
 
-  // if(event.target) {
+// if(event.target) {
 
-  // }
+// }
 // }
 
 // gameState.board[0][0] = 'x'
 
-// gameState.board[2][2] = 'o' 
+// gameState.board[2][2] = 'o'
 
-// gameState.board[1][1] = gameState.players[0] 
+// gameState.board[1][1] = gameState.players[0]
 
-
-//   console.log(gameState) 
-
-
+//   console.log(gameState)
 
 //  Element.addEventListener('click', function(event)) {
 
 //  }
 
-
-
 // find a way to get into the object - to try to get the board to another value
 
-
-  //    make functions that allow when each player takes their turn, either A or X 
-
-
-
+//    make functions that allow when each player takes their turn, either A or X
 
 // To help:
 
@@ -91,6 +92,4 @@ gameZone.addEventListener ("click", function (event) {
 
 // getElementsByTagName find nodes with a certain HTML tag
 
-//querySelector search using CSS selectors 
-
-
+//querySelector search using CSS selectors
